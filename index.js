4386cd5271b8ea8e0d0cdbc2cdf2b338b385f297
warlikerichard <warlikerichard@hotmail.com>
2023-07-28 14:04:48 -0300

    Functional connection - handles receive and send message

--- index.js
+++ index.js
@@ -19,6 +19,19 @@ const PORT = 3001;
 
 io.on('connection', socket => {
     console.log('Usuário conectado!', socket.id);
+
+    socket.on('disconnect', reason => {
+        console.log('User disconnected:', reason);
+    })
+
+    socket.on('set_username', username => {
+        socket.data.username = username;
+        console.log(socket.data.username);
+    })
+
+    socket.on('message', text => {
+        io.emit('receive_message', {text, authorId: socket.id, author: socket.data.username})
+    })
 })
 
 server.listen(PORT, () => console.log('Server running!'));
